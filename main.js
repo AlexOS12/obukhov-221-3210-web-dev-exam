d404cb7151e44b2643a85abaa248259c0d9f1cd3
AlexOS12 <sluhach2016@gmail.com>
2024-01-04 12:34:54 +0300

    Реализован переход на страницу аккаунта (сама страница еще нет)

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let accountBtn = document.getElementById("accBtn");
+    accountBtn.onclick = function () {
+        window.location.href = "account.html";
+    };
+};
